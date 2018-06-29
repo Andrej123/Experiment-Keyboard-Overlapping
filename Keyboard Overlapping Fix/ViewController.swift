fb0f87862d0e5291bc69b249babff581f70f6471
Andrej Krizmancic <andrej.krizmancic@gmail.com>
2018-06-29 11:17:00 +0200

    Keyboard overlaps the cells.

--- Keyboard Overlapping Fix/ViewController.swift
+++ Keyboard Overlapping Fix/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet fileprivate var titleLabel: UILabel!
+	@IBOutlet fileprivate var xButton: UIButton!
+	@IBOutlet fileprivate var sendButton: UIButton!
+	@IBOutlet fileprivate var tableView: UITableView!
+	@IBOutlet fileprivate var textField: UITextField!
+
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	@IBAction fileprivate func xButtonTapped() {
+		print("\(#function)")
+	}
+
+	@IBAction fileprivate func sendButtonTapped() {
+		print("\(#function)")
 	}
 
 
 }
 
+
+extension ViewController: UITableViewDataSource {
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 18
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		cell.backgroundColor = indexPath.row % 2 == 0 ? .red : .yellow
+		cell.textLabel?.text = "Row \(indexPath.row)"
+		return cell
+	}
+
+}
+
+
+extension ViewController: UITableViewDelegate {
+
+}
+
+
+extension ViewController: UITextFieldDelegate {
+
+}
